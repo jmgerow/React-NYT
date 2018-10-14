b33ec7b05594a5c9a8b1fce5a5ba0b4bed6633f1
jmgerow <justingerow1@gmail.com>
2018-10-14 14:56:44 -0500

    added results field

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 // import DeleteBtn from "../../components/DeleteBtn";
 import Jumbotron from "../../components/Jumbotron";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
-// import { List, ListItem } from "../../components/List";
+import { List, ListItem } from "../../components/List";
 import { Input, FormBtn } from "../../components/Form";
 
 
@@ -13,47 +13,70 @@ class Home extends Component {
         topic: "",
         startYear: "",
         endYear: ""
-      };
+    };
     render() {
         return (
-          <Container>
-            <Row>
-              <Col size="md-12">
-                <Jumbotron>
-                  <h1>Search for Articles</h1>
-                </Jumbotron>
-                <form>
-                  <Input
-                    value={this.state.topic}
-                    onChange={this.handleInputChange}
-                    name="topic"
-                    placeholder="Topic (required)"
-                  />
-                  <Input
-                    value={this.state.startYear}
-                    onChange={this.handleInputChange}
-                    name="startYear"
-                    placeholder="Start Year (required)"
-                  />
-                  <Input
-                    value={this.state.endYear}
-                    onChange={this.handleInputChange}
-                    name="endYear"
-                    placeholder="End Year (required)"
-                  />
-                  <FormBtn
-                    disabled={!(this.state.author && this.state.title)}
-                    onClick={this.handleFormSubmit}
-                  >
-                    Search
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                        <Jumbotron>
+                            <h1>Search for Articles</h1>
+                        </Jumbotron>
+                        <form>
+                            <Input
+                                value={this.state.topic}
+                                onChange={this.handleInputChange}
+                                name="topic"
+                                placeholder="Topic (required)"
+                            />
+                            <Input
+                                value={this.state.startYear}
+                                onChange={this.handleInputChange}
+                                name="startYear"
+                                placeholder="Start Year (required)"
+                            />
+                            <Input
+                                value={this.state.endYear}
+                                onChange={this.handleInputChange}
+                                name="endYear"
+                                placeholder="End Year (required)"
+                            />
+                            <FormBtn
+                                // disabled={!(this.state.author && this.state.title)}
+                                onClick={this.handleFormSubmit}
+                            >
+                                Search
                   </FormBtn>
-                </form>
-              </Col>
-              
-            </Row>
-          </Container>
+                        </form>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col size="md-12">
+                        <Jumbotron>
+                            <h1>Results</h1>
+                        </Jumbotron>
+                        {this.state.articles.length ? (
+                            <List>
+                                {this.state.books.map(book => (
+                                    <ListItem key={book._id}>
+                                        <Link to={"/books/" + book._id}>
+                                            <strong>
+                                                {book.title} by {book.author}
+                                            </strong>
+                                        </Link>
+                                        {/* <DeleteBtn onClick={() => this.deleteBook(book._id)} /> */}
+                                    </ListItem>
+                                ))}
+                            </List>
+                        ) : (
+                                <h3>No Results to Display</h3>
+                            )}
+                    </Col>
+                </Row>
+            </Container>
         );
-      }
+    }
 
 
 };
