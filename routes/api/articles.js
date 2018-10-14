821b8edb6871919bd11aa01538a5235aa2b1703c
jmgerow <justingerow1@gmail.com>
2018-10-14 14:26:52 -0500

    display nav

---
+++ routes/api/articles.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+// const articlesController = require("../../controllers/articlesController");
+
+// root
+router.route("/")
+  
+
+
+
+module.exports = router;
