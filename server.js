cfecd440c95e57ef763c8e0ebf92c6521614fa7a
jmgerow <justingerow1@gmail.com>
2018-10-20 21:07:53 -0500

    added mlab

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactnytscraper");
 
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
