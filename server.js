eff1fb3dcc54405284af863dd19e2c2286f0b051
jmgerow <justingerow1@gmail.com>
2018-10-20 21:16:52 -0500

    fixed error in server for mongo setup

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactnytscraper");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactnytscraper";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
